c420e7b0f8c983b2e4f33850bce9c089ba9dad7c
samirazazy <samiralazazy@gmail.com>
2021-04-06 14:25:32 +0200

    Crud routes

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,3 +1,13 @@
-exports.getAllNotes = (req, res) => {
-  res.send('get all notes');
+exports.getAllNotes = (req, res) => res.send('get all notes');
+
+exports.saveNote = (req, res) => {
+  res.send('save note');
+};
+
+exports.updateAllNote = (req, res) => {
+  res.send('update note');
+};
+
+exports.deleteAllNote = (req, res) => {
+  res.send('delete note');
 };
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -4,5 +4,8 @@ var router = express.Router();
 const noteController = require('../controller/noteController');
 
 router.get('/notes', noteController.getAllNotes);
+router.post('/note/save', noteController.saveNote);
+router.put('/note/update', noteController.updateAllNote);
+router.delete('/note/delete', noteController.deleteAllNote);
 
 module.exports = router;
