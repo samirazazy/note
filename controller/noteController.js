59c5eb92d81eb75a320188794bb1467d935f657f
samirazazy <samiralazazy@gmail.com>
2021-04-11 16:47:24 +0200

    34- Node JS - Create new Note | SaveNote function using HTTP Post

--- controller/noteController.js
+++ controller/noteController.js
@@ -3,25 +3,37 @@ const memStorage = require('../util/memory.storage');
 const model = require('../model/note.model');
 
 exports.getAllNotes = (req, res) => {
-  const seqId = generator.generate();
-  memStorage.store.setItem(seqId, '1st key');
-
-  const seqId2 = generator.generate();
-  memStorage.store.setItem(seqId2, '2st key');
-
-  const keys = memStorage.getKyes(memStorage.store);
-  const values = memStorage.getValues(memStorage.store);
-
-  const Note = model.Note;
-  let noteObj = new Note(seqId, 'dd', 'dd', 'dd', new Date());
-
-  console.log(noteObj);
-  //   console.log(keys, values);
-  console.log(JSON.stringify(keys), JSON.stringify(values));
-  res.send('get all notes' + seqId);
+  // const seqId = generator.generate();
+  // memStorage.store.setItem(seqId, '1st key');
+  // const seqId2 = generator.generate();
+  // memStorage.store.setItem(seqId2, '2st key');
+  // const keys = memStorage.getKyes(memStorage.store);
+  // const values = memStorage.getValues(memStorage.store);
+  // const Note = model.Note;
+  // let noteObj = new Note(seqId, 'dd', 'dd', 'dd', new Date());
+  // console.log(noteObj);
+  // //   console.log(keys, values);
+  // console.log(JSON.stringify(keys), JSON.stringify(values));
+  // res.send('get all notes' + seqId);
 };
 exports.saveNote = (req, res) => {
-  res.send('save note');
+  const seqId = generator.generate();
+  const createdBy = 'admin';
+  const createdOn = new Date();
+
+  //req body
+  let title = req.body.title;
+  let content = req.body.content;
+  if (!title || !content) {
+    return res
+      .status(500)
+      .send({ errors: `title or content shouldn't be empty!` });
+  }
+
+  const Note = model.Note;
+  let noteObj = new Note(seqId, title, content, createdBy, createdOn);
+  memStorage.store.setItem(seqId, noteObj);
+  return res.status(201).send('created successfully');
 };
 
 exports.updateAllNote = (req, res) => {
