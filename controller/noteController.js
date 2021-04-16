e83423bc14959d1815b1931cce1f76c4bffd8dfd
samirazazy <samiralazazy.com>
2021-04-16 15:42:28 +0200

    36- Node JS -  Update Note _ Update Selected note using HTTP PUT

--- controller/noteController.js
+++ controller/noteController.js
@@ -15,11 +15,12 @@ exports.saveNote = (req, res) => {
   //req body
   let title = req.body.title;
   let content = req.body.content;
-  if (!title || !content) {
+
+  if (!title || !content)
     return res
       .status(500)
       .send({ errors: `title or content shouldn't be empty!` });
-  }
+
   const Note = model.Note;
   let noteObj = new Note(seqId, title, content, createdBy, createdOn);
   memStorage.store.setItem(seqId, noteObj);
@@ -27,7 +28,30 @@ exports.saveNote = (req, res) => {
 };
 
 exports.updateAllNote = (req, res) => {
-  res.send('update note');
+  const createdBy = 'admin';
+  const createdOn = new Date();
+
+  //req body
+  let noteId = req.body.noteId;
+  let title = req.body.title;
+  let content = req.body.content;
+
+  if (!noteId)
+    return res.status(500).send({ errors: ` noteId shouldn't be empty!` });
+
+  if (!title || !content) {
+    return res
+      .status(500)
+      .send({ errors: `title or content shouldn't be empty!` });
+  }
+
+  if (!memStorage.store.getItem(noteId))
+    return res.status(500).send({ errors: `noteId is not exist` });
+
+  const Note = model.Note;
+  let noteObj = new Note(noteId, title, content, createdBy, createdOn);
+  memStorage.store.setItem(noteId, noteObj);
+  return res.status(200).send('updated successfully');
 };
 
 exports.deleteAllNote = (req, res) => {
